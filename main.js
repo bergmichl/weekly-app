3f2c6bde0b583d8069b4241cdfb499f4a523b4d3
Michael Bergmüller <michaelbergmueller@hotmail.com>
2021-05-06 13:43:44 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const dateEl = document.querySelector('.date');
+const moreInfo = document.getElementById('moreInfo');
+const infoEl = document.getElementById('info');
+const addBtn = document.getElementById('add');
+const btnsContainer = document.querySelector('.btns-container');
+
+// Get Date and output it on the DOM
+const weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'];
+let today = new Date();
+let weekday = weekdays[today.getDay() - 1];
+dateEl.innerText = `${weekday}, ${today.getDate()}.${+today.getMonth() + 1}.${today.getFullYear()}`;
+
+// ----- EVENTS ------
+moreInfo.addEventListener('click', () => infoEl.classList.toggle('hidden'));
+addBtn.addEventListener('click', addInputToDOM);
+
+// ----- FUNCTIONS -----
+function addInputToDOM() {
+  let newInputEl = document.createElement('input');
+  newInputEl.setAttribute('type', 'text');
+  btnsContainer.parentNode.insertBefore(newInputEl, btnsContainer)
+  newInputEl.focus()
+}
+
+
+
+// TODO:
+// Wenn ein Input gefüllt wird, dann daumen-emoji dahinter
+// Choose a font (CSS)
---
+++ readme.md
@@ -0,0 +1,23 @@
+# What it's supposed to be:
+
+* Write down all the positive things that happend to you during the day or rather you did well.
+
+* This is supposed to act as a daily reminder to be positive and be aware of good things happening, even the smallest things.
+
+* This is meant to shift your focus to good things and keep/make you happy and thankful.
+
+* Especially if you don't feel good, you can have a look back into the recent past. 
+
+# Supposed to be implemented:
+
+* Notes are supposed to be stored in the browser-local-storage.
+
+* There should be a minimum of 3-5 daily notes before the form can be submitted.
+
+* There should be some sort of view - to have an overview of the past weeks.
+
+* There should be a random reminder of some past notes every once in a while.
+
+* Little effects and animations should be included in the general UI.
+
+<hr>
