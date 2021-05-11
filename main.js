40f59c3e338d441883f61c6fb1fc0ab070f5ee0e
Michael Bergmüller <michaelbergmueller@hotmail.com>
2021-05-11 19:28:33 +0200

    added nav.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const containers = document.querySelectorAll('.container');
+const toggleNavBtn = document.getElementById('toggle-nav');
+const navEl = document.querySelector('nav');
 const dateEl = document.querySelector('.date');
 const labelEls = document.querySelectorAll('label[for=mood]');
 const rangeInput = document.getElementById('mood');
@@ -6,6 +9,20 @@ const infoEl = document.getElementById('info');
 const addBtn = document.getElementById('add');
 const btnsContainer = document.querySelector('.btns-container');
 
+// Navigation
+const li_today = document.getElementById('li-today');
+li_today.addEventListener('click', function() {
+  removeAllContainers();
+  containers[0].classList.remove('d-none');
+  toggleNav();
+})
+const li_week = document.getElementById('li-week');
+li_week.addEventListener('click', function() {
+  removeAllContainers();
+  containers[1].classList.remove('d-none');
+  toggleNav();
+})
+
 // Get Date and output it on the DOM
 const weekdays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'];
 let today = new Date();
@@ -13,6 +30,7 @@ let weekday = weekdays[today.getDay() - 1];
 dateEl.innerText = `${weekday}, ${today.getDate()}.${+today.getMonth() + 1}.${today.getFullYear()}`;
 
 // ----- EVENT-LISTENERS ------
+toggleNavBtn.addEventListener('click', toggleNav);
 rangeInput.addEventListener('input', updateEmoji);
 moreInfo.addEventListener('click', () => infoEl.classList.toggle('d-none'));
 addBtn.addEventListener('click', addInputToDOM);
@@ -30,6 +48,17 @@ function updateEmoji() {
   labelEls[rangeInput.value - 1].classList.remove('v-hidden');
 }
 
+function toggleNav() {
+  navEl.classList.toggle('d-none');
+}
+
+function removeAllContainers() {
+  containers.forEach((container) => {
+    container.classList.add('d-none');
+  });
+}
 
 
 // const inputs = document.querySelectorAll('input[type=text]'); ADD "Daumenemoji" after filled inputs 👍
+// LOCAL STORAGE!
+// Display LS on "week-page"
