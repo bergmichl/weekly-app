258158f7996a784eb569b7dd2db739e855d0287f
Michael Bergmüller <michaelbergmueller@hotmail.com>
2021-05-18 16:35:09 +0200

    get data from LS.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const containers = document.querySelectorAll('.container');
 const containerToday = document.querySelector('.container-today');
+const containerWeek = document.querySelector('.container-week');
 const toggleNavBtn = document.getElementById('toggle-nav');
 const navEl = document.querySelector('nav');
 const dateEl = document.querySelector('.date');
@@ -11,6 +12,11 @@ const inputs = document.querySelectorAll('input[type=text]');
 const btnsContainer = document.querySelector('.btns-container');
 const submitBtn = document.getElementById('submit');
 const addBtn = document.getElementById('add');
+const week = document.querySelector('.week');
+
+const moodColors = [
+  'red', 'orangered', 'orange', 'limegreen', 'lawngreen'
+];
 
 // Navigation
 const li_today = document.getElementById('li-today');
@@ -74,6 +80,7 @@ function submitTodayLS() {
 
   localStorage.setItem('weekly', JSON.stringify(entry));
   checkStatus();
+  window.location.reload();
 }
 
 function getCheckedFields() {
@@ -91,12 +98,50 @@ function getCheckedFields() {
 // Check if daily entry has been made
 function checkStatus() {
   let lastEntry = JSON.parse(localStorage.getItem('weekly'));
-  let lastEntryDate = lastEntry[0].date;
+  if(lastEntry === null) {
+    return;
+  } else {
+    let lastEntryDate = lastEntry[0].date;
 
-  if(dateEl.innerText == lastEntryDate) {
-    let messageNode = document.createElement('h1');
-    messageNode.innerHTML = 'Super - du hast den heutigen Eintrag erledigt!';
-    containerToday.innerHTML = '';
-    containerToday.appendChild(messageNode);
-  } else return;
+    if(dateEl.innerText == lastEntryDate) {
+      let messageNode = document.createElement('h1');
+      messageNode.innerHTML = 'Super - du hast den heutigen Eintrag bereits erledigt!';
+      containerToday.innerHTML = '';
+      containerToday.appendChild(messageNode);
+    } else return;
+  }
+  
+}
+
+// output LS data to WEEK-page
+let weeklyDB = JSON.parse(localStorage.getItem('weekly'));
+if(weeklyDB != null) {
+  if(weeklyDB.length > 8) {
+    const btns = document.querySelector('.container-week .btns-container');
+    btns.classList.remove('d-none');
+  }
+
+  weeklyDB.forEach((day) => {
+    let div = document.createElement('div');
+    div.classList.add('day');
+    div.style.backgroundColor = moodColors[day.mood - 1];
+
+    let h3 = document.createElement('h3');
+    h3.innerText = day.date;
+    let ul = document.createElement('ul');
+    day.checks.forEach((check) => {
+      let li = document.createElement('li');
+      li.innerText = check;
+      ul.appendChild(li);
+    })
+
+    week.appendChild(div);
+    div.appendChild(h3);
+    div.appendChild(ul);
+  })
+
+} else {
+    let messageNode = document.createElement('div');
+    messageNode.innerHTML = '<h1 class="message">Es sind noch keine Einträge vorhanden!</h1><p>Die App speichert nur Daten im jeweiligen Browser, wo sie aufgerufen wird und nur am jeweiligen Gerät.</p>';
+    containerWeek.appendChild(messageNode);
 }
